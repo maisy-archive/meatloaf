3f2df6f8173a2e38a9edeb68cbabde60cc2beec5
Beefers <Beefers@users.noreply.github.com>
2021-09-27 06:58:15 +0000

    deploy: 97b19e3d62bb1a4be08007a27a40052b97d406f3

--- QuickCSS/plugin.js
+++ QuickCSS/plugin.js
@@ -1,7 +1,7 @@
-(function(r,u,t){"use strict";function a(c){const e=document.createElement("style");return e.className="CUMCORD_INJECTED_CSS CUMCORD_QUICK_CSS",e.textContent=c,document.head.appendChild(e),()=>{e.remove()}}function o(){for(const c of document.querySelectorAll(".CUMCORD_QUICK_CSS"))c.remove()}const m=u.webpackModules.findByDisplayName("TextArea");let s;var d=c=>{const e=c.persist.store;return{onLoad(){//! Deprecated settings patch
-s=r.patcher.injectCSS(`
+(function(a,n,u,t){"use strict";function i(e){const c=document.createElement("style");return c.className="CUMCORD_INJECTED_CSS CUMCORD_QUICK_CSS",c.textContent=e,document.head.appendChild(c),()=>{c.remove()}}function s(){for(const e of document.querySelectorAll(".CUMCORD_QUICK_CSS"))e.remove()}const l=n.webpackModules.findByDisplayName("TextArea");var m=e=>{u.useNest(e.data.persist);const c=e.data.persist.store;return t.React.createElement("div",{className:"cumcord-quickcss"},t.React.createElement(FormTitle,{className:"cumcord-quickcss-formtitle",tag:"h1"},"Quick CSS"),t.React.createElement(l,{className:"cumcord-quickcss-input",rows:20,value:c.css,onChange:r=>{s(),i(r),c.css=r,console.log(c.css)}}))};let o;var d=e=>({onLoad(){//! Deprecated settings patch
+o=a.patcher.injectCSS(`
                 .cumcord-quickcss-formdivider, .cumcord-quickcss-helpmessage {
                     margin-bottom: 20px;
                     margin-top: 20px;
                 }
-            `)},onUnload(){s(),o()},settings:t.React.createElement("div",{className:"cumcord-quickcss"},t.React.createElement(m,{rows:20,onChange:n=>{o(),a(n),e.css=n}}))}};return d})(cumcord,cumcord.modules,cumcord.modules.common);
+            `)},onUnload(){o(),s()},settings:m(e)});return d})(cumcord,cumcord.modules,cumcord.utils,cumcord.modules.common);
