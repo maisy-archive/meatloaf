9c86381f644645f32f125c62fed1c1c6125c1fea
Beef <ethandavidbarrow@outlook.com>
2021-10-26 19:51:41 +0100

    [ConceptArts] I am tired of this plugin

--- ConceptArts/src/components/Settings.jsx
+++ ConceptArts/src/components/Settings.jsx
@@ -1,15 +1,25 @@
-import { findByDisplayName } from "@cumcord/modules/webpack";
-const DropdownButton = findByDisplayName("DropdownButton");
+import { findByProps } from "@cumcord/modules/webpack";
+
+const Button = findByProps(
+    "Sizes",
+    "Colors",
+    "Looks",
+    "DropdownSizes"
+);
+const { openModal } = findByProps("openModal", "openModalLazy");
+
+import DiscoinModal from "./modals/discoins/Modal";
 
 export function Settings(props) {
     return (
         <>
-            <DropdownButton 
+            <Button 
                 size="sizeMedium-1AC_Sl"
                 color="colorBrand-3pXr91"
+                onClick={DiscoinModal()}
             >
                 Open Discoin Modal
-            </DropdownButton>         
+            </Button>         
         </>
     )
 }
