776ed9b29f9ff5d3195794282e322ba0169b1968
Beefers <Beefers@users.noreply.github.com>
2021-11-22 21:03:31 +0000

    deploy: ebed70e9bfb5d167739bc53874fc43cbabe8c1bb

--- Cumstain/plugin.js
+++
@@ -1,1 +0,0 @@
-(function(e,t){"use strict";const n=t.findByDisplayName("FormTitle");function r(c){return e.React.createElement(e.React.Fragment,null,e.React.createElement(n,{tag:"h2"},"Themer"))}var a=c=>({onLoad(){},onUnload(){},settings:e.React.createElement(r)});return a})(cumcord.modules.common,cumcord.modules.webpack);
