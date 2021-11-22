553abcfbca8e29892bbc6d69130c09b6bb645c4a
Cain Atkinson <yellowsink@protonmail.com>
2021-11-22 21:18:28 +0000

    gigantic quickcss code cleanup

---
+++ QuickCSS/src/index.js
@@ -0,0 +1,19 @@
+import { injectCSS } from "@cumcord/patcher";
+import data from "@cumcord/pluginData";
+import styles from "./styles.css";
+import Settings from "./Settings";
+
+import { createElement } from "@cumcord/modules/common/React";
+
+export default (data) => {
+  data.patchedQuickCSS = injectCSS(data.persist.ghost.css);
+  let editorStyles = styles();
+
+  return {
+    onUnload() {
+      data.patchedQuickCSS();
+      editorStyles();
+    },
+    settings: createElement(Settings),
+  };
+};
