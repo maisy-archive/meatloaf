104292095ccb5c9f75542f524714bbba1559fef7
Phorcys <57866459+phorcys420@users.noreply.github.com>
2022-04-01 23:33:08 +0200

    Merge from PrematureEjaculator - Backup - Snippet - Restore - Unify lib functions

--- MigrationStation/src/Settings.jsx
+++ MigrationStation/src/Settings.jsx
@@ -1,9 +1,11 @@
-import createBackup from "./createBackup";
-import downloadFile from "./downloadFile";
-import uploadFile from "./uploadFile";
+import { copyText, saveFile, openFile, showToast } from "./lib/util";
+import { getPlugins, loadPlugins, makeSnippet } from "./lib/plugins";
+
 import SettingsCard from "./components/SettingsCard";
 
-export default function Settings(props) {
+import logger from "@cumcord/utils";
+
+export default function Settings() {
     return (
         <>
             <div className="beef-ms-settings-card-container">
@@ -12,14 +14,41 @@ export default function Settings(props) {
                     description="Creates a backup of your current installation, including plugins and plugin nests." 
                     buttonText="Backup"
                     buttonSize="MEDIUM"
-                    buttonFunc={async (e) => { downloadFile(JSON.stringify(await createBackup()), "backup.json") }}
+                    buttonFunc={async () => { await saveFile(JSON.stringify(await getPlugins()), `backup-${Date.now()}.json`) }}
+                />
+                <SettingsCard 
+                    title="make big snippet very cool" 
+                    description="phorcys does not know how to make a submenu with react" 
+                    buttonText="Copy"
+                    buttonSize="MEDIUM"
+                    buttonFunc={async () => {
+                        try {
+                            await copyText(await makeSnippet());
+                        } catch(e) {
+                            showToast({message: "Could not copy to clipboard! :(", id: "ms-clipboard-error", type: 2});
+                            logger.error("[MigrationStation]", "Could not copy to clipboard", e);
+                        } finally {
+                            showToast({message: "Snippet was copied to clipboard successfully!", id: "ms-clipboard-success", type: 1});
+                        }
+                    }}
                 />
                 <SettingsCard 
                     title="Restore" 
                     description="Restores from a backup of your current installation." 
                     buttonText="Restore"
                     buttonSize="MEDIUM"
-                    buttonFunc={async (e) => { console.log(uploadFile()) }}
+                    buttonFunc={async () => {
+                        let json;
+
+                        try {
+                            json = JSON.parse(await openFile());
+                        } catch(e) {
+                            showToast({message: "Could not open backup! :(", id: "ms-backup-error", type: 2});
+                            logger.error("[MigrationStation]", "Could not open backup", e);
+                        } finally {
+                            loadPlugins(json);
+                        }
+                    }}
                 />
             </div>
         </>
---
+++ MigrationStation/src/lib/plugins.js
@@ -0,0 +1,65 @@
+import { showToast as utilShowToast } from "./util";
+
+import idb from "@cumcord/modules/internal/idbKeyval";
+import installedPlugins from "@cumcord/plugins/installed";
+
+export async function getPlugins() {
+    let plugins = [];
+
+    for (const [url, data] of Object.entries(installedPlugins.store || {})) {
+        let plugin = {
+            name: data.manifest.name,
+            enabled: data.enabled,
+            url
+        }
+        
+        let keyval = await idb.get(`${url}_CUMCORD_STORE`);
+        if(keyval) plugin.settings = keyval;
+
+        plugins.push(plugin);
+    }
+
+    return plugins;
+}
+
+// in here, we HAVE to use window.cumcord, otherwise the snippet functionality would not be able to access
+export async function loadPlugins(plugins, showToast = utilShowToast) {
+    for(let plugin of plugins) {
+        if(plugin.settings) {
+            await cumcord.modules.internal.idbKeyval.set(`${plugin.url}_CUMCORD_STORE`, plugin.settings);
+        }
+
+        if(!cumcord.plugins.installed.ghost[plugin.url]) {
+            try {
+                await cumcord.plugins.importPlugin(plugin.url);
+            } catch(e) {
+                cumcord.utils.logger.error("[MigrationStation]", "Could not load plugin", plugin.url, e);
+                showToast({message: `Could not load ${plugin.name}, check the console.`, id: `ms-${plugin.url}-err`, type: 2});
+            }
+
+            if(plugin.enabled == false) cumcord.plugins.togglePlugin(plugin.url);
+        } else {
+            // Reload plugin for idb changes to take effect
+            try {
+                cumcord.plugins.togglePlugin(plugin.url);
+                cumcord.plugins.togglePlugin(plugin.url);
+            } catch(e) {
+                cumcord.utils.logger.error("[MigrationStation]", "Could not reload plugin", plugin.name, e);
+                showToast({message: `Could not reload ${plugin.name}, check the console.`, id: `ms-${plugin.url}-err`, type: 2});
+            }
+        }
+    }
+
+    showToast({message: "Finished loading backup!", id: "ms-finish", type: 1});
+
+    // Reload window for idb changes to take effect, does not appear to be needed if you reload plugin.
+    // window.location.reload();
+}
+
+export async function makeSnippet(plugins) {
+    /*
+        kinda ugly but the only workaround I found to the fact that showToast is non-existant if you only have the function above's code
+        and yes i am casting a function into a string
+    */
+    return `(${loadPlugins})(${JSON.stringify(plugins ?? await getPlugins())}, cumcord.modules.webpack.findByProps("showToast").showToast)`;
+}
