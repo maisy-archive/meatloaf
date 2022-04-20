117d64e204eca4f712afde2d536075a04dc8dd76
Phorcys <57866459+phorcys420@users.noreply.github.com>
2022-04-20 16:23:52 +0200

    [Superpowers] Make Spotify No Autopose use cc's patcher & remove 1 useless patch

--- Superpowers/src/lib/modules/spotifyNoAutopause.js
+++ Superpowers/src/lib/modules/spotifyNoAutopause.js
@@ -1,16 +1,14 @@
 import { findByProps } from "@cumcord/modules/webpack";
+import { instead } from "@cumcord/patcher";
 
 const spotifyPlayer = findByProps("SpotifyAPI");
-const spotifyModule = findByProps("wasAutoPaused");
-const { pause }         = spotifyPlayer;
-const { wasAutoPaused } = spotifyModule;
+
+let patch;
 
 export default function spotifyNoAutopause(enabled) {
     if (enabled === true) {
-        spotifyModule.wasAutoPaused = () => false;
-        spotifyPlayer.pause = () => void 0;
+        patch = instead("pause", spotifyPlayer, () => void 0);
     } else {
-        spotifyModule.wasAutoPaused = wasAutoPaused;
-        spotifyPlayer.pause = pause;
+        patch();
     }
 }
