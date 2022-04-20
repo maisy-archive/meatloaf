4af7710f5c5b33f264d1aa45bf8e46fdeae0de24
Phorcys <57866459+phorcys420@users.noreply.github.com>
2022-04-20 16:46:43 +0200

    [Superpowers] Make Infinite Accounts code smaller

--- Superpowers/src/lib/modules/infiniteAccounts.js
+++ Superpowers/src/lib/modules/infiniteAccounts.js
@@ -3,9 +3,5 @@ import { findByProps } from "@cumcord/modules/webpack";
 let { MAX_ACCOUNTS } = findByProps("MAX_ACCOUNTS"); // original
 
 export default function infiniteAccounts(enabled) {
-    if(enabled === true) {
-        findByProps("MAX_ACCOUNTS").MAX_ACCOUNTS = Infinity;
-    } else {
-        findByProps("MAX_ACCOUNTS").MAX_ACCOUNTS = MAX_ACCOUNTS;
-    }
+    findByProps("MAX_ACCOUNTS").MAX_ACCOUNTS = enabled ? Infinity : MAX_ACCOUNTS
 }
