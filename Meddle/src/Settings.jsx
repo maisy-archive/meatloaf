e0c6952343cf5c652efed55c6e694ccaa3cc1f71
Beef <beefers@riseup.net>
2022-05-25 08:04:42 +0100

    [Meddle] Experimental settings rework

--- Meddle/src/Settings.jsx
+++ Meddle/src/Settings.jsx
@@ -2,18 +2,22 @@ import { FormDivider } from "./WPMODULES";
 
 import { ErrorBoundary } from "@cumcord/ui/components";
 
+import TabBar from "./components/TabBar";
 import SettingsTitle from "./components/SettingsTitle";
-import DispatchSettings from "./components/DispatchSetttings";
+import TabDispatch from "./components/TabDispatch";
 
 export default function Settings(props) {
     return (
         <ErrorBoundary>
             <div className="beef-meddle-settings">
                 <SettingsTitle />
-
                 <FormDivider className="beef-meddle-settings-divider" />
 
-                <DispatchSettings />
+                <TabBar
+					items={[
+						{ text: "FluxDispatcher", component: TabDispatch },
+					]}
+				/>
             </div>
         </ErrorBoundary>
     );
--- Meddle/src/components/TabDispatch.jsx
+++ Meddle/src/components/TabDispatch.jsx
@@ -1,10 +1,8 @@
 import { SSwitch, SText } from "cumcord-tools";
-import { FormTitle } from "../WPMODULES";
 
 export default function DispatchSettings(props) {
     return (
-        <div className="beef-meddle-settings-section">
-            <FormTitle tag="h3">FluxDispatcher</FormTitle>
+        <div className="beef-meddle-settings-tab">
             <SSwitch k="dispatchLogEnabled">Log all FluxDispatcher calls to console</SSwitch>
             <SSwitch k="dispatchWhiteListMode">Dispatch logger whitelist mode</SSwitch>
             <SText
