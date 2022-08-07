248fa5d58e498cddb068361a96d7b3dfff6c3cb4
Beef <beefers@riseup.net>
2022-08-07 21:35:01 +0100

    [Sapphire > Patcher] Fix weird indent

--- plugins/Sapphire/src/lib/patcher/index.js
+++ plugins/Sapphire/src/lib/patcher/index.js
@@ -2,7 +2,7 @@ import * as base from "./base";
 import * as commands from "./commands";
 import internalMessage from "./internalMessage";
 
-            // TODO: Polyfill all the "extra" stuff that's in GM's patcher, see https://github.com/GooseMod/GooseMod/tree/master/src/util/patcher
+// TODO: Polyfill all the "extra" stuff that's in GM's patcher, see https://github.com/GooseMod/GooseMod/tree/master/src/util/patcher
 export default {
     ...base,
     commands: commands,
