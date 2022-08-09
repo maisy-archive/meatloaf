8c3cc824de79f0e1906b6e760161c45b6afa4799
Beef <beefers@riseup.net>
2022-08-09 02:40:45 +0100

    [Sapphire] Properly handle when GooseMod is already running

--- plugins/Sapphire/src/index.js
+++ plugins/Sapphire/src/index.js
@@ -3,12 +3,38 @@ import loadTopaz from "./patches/loadTopaz";
 import fixLocalStorage from "./patches/fixLocalStorage";
 import injectStyles from "./styles.scss";
 
-const patches = [
-    fixLocalStorage(),
-    registerGoosemodObj(),
-    loadTopaz(),
-    injectStyles(),
-]
+import { showToast } from "@cumcord/ui/toasts";
+import { installed, togglePlugin } from "@cumcord/plugins";
+import { id } from "@cumcord/pluginData";
+
+function tryDisable() {
+    setTimeout(() => {
+        if (installed.ghost[id]?.enabled) {
+            togglePlugin(id);
+        } else {
+            tryDisable();
+        }
+    }, 1000);
+}
+
+let patches = [];
+
+if (!window.goosemod && !window.goosemod?._SAPPHIRE) {
+    patches = [
+        registerGoosemodObj(),
+        fixLocalStorage(),
+        loadTopaz(),
+        injectStyles(),
+    ]
+} else {
+    showToast({
+        title: "Sapphire",
+        content: "GooseMod is already running! Sapphire will be disabled, as it will only cause incompatibilities.",
+
+    });
+
+    tryDisable();
+}
 
 export function onUnload() {
     _.forEachRight(patches, (p) => p());
